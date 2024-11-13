896f3e458c091dda32d6be7b608df9b36f6fcd45
RandyAtem <evitaatem@gmail.com>
2024-11-12 19:04:13 -0500

    Update load-test.sh
    Ensuring that the load-test.sh script correctly references the local ports after port-forwarding.

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting load test for foo.localhost..."
-hey -z 30s -c 10 http://foo.localhost > load-test-result.txt
+hey -z 30s -c 10 http://localhost:8080 > load-test-result.txt
 
 echo "Starting load test for bar.localhost..."
-hey -z 30s -c 10 http://bar.localhost >> load-test-result.txt
+hey -z 30s -c 10 http://localhost:8081 >> load-test-result.txt
